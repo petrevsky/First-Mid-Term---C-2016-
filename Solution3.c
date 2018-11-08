e57cd495cc7bb7826072a3e5456ecd172eb6327e
petrevsky <chrispetrevsky@gmail.com>
2018-11-08 01:52:45 +0100

    Update Solution3.c

--- Solution3.c
+++ Solution3.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
+
 int main()
 {
-
-    int n = 20;
-
-    do {
-            n /= 10;
-    int digit = n % 10;
-    putchar('0' + digit);
-
-} while (n > 0);
-
-printf("%d", n);
+    int enterNumber;
+    int tempMSD;
+    int tempLSD;
 
 
+    while (scanf("%d", &enterNumber)) {
+        tempLSD = enterNumber % 10;
+
+        while (enterNumber > 10) {
+            enterNumber /= 10;
+        }
+        tempMSD = enterNumber;
+        if (tempMSD < tempLSD) {
+            printf("The MSD is %d and the LSD is %d.", tempMSD, tempLSD);
+            printf(" %d %d\n", tempMSD, tempLSD);
+        }
+
+
+}
 
 }
